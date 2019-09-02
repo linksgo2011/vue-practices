9c8a6a40de531d429ba1813c85f3ca70c93d60d4
Lin Ning <nlin@thoughtworks.com>
2019-09-02 18:02:00 +0800

    add todo list axios

---
+++ vue-todo-list-axios/src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="items">
+        <template v-for="(item,index) in todoList">
+            <li :key="index" :class="item.status" v-show="item.status === currentFilter || currentFilter === 'all'">
+                <input name="status-toggle" type="checkbox" @change="toggleActive(index)">
+                <a href="" class="content">{{item.content}}</a>
+            </li>
+        </template>
+    </ul>
+</template>
+
+<script>
+    import {mapState, mapMutations,mapActions} from "vuex";
+
+    export default {
+        name: "todo-list",
+        computed: {
+            ...mapState(['todoList','currentFilter'])
+        },
+        methods: {
+            ...mapMutations(['toggleActive']),
+            ...mapActions(['fetchTodos']),
+        },
+        created(){
+            this.fetchTodos();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-todo-list-axios/src/components/TodoListFilter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="filter">
+        <a @click.prevent="setFilter('all')" :class="{active:currentFilter=='all'}">ALL</a>
+        <a @click.prevent="setFilter('active')" :class="{active:currentFilter=='active'}">Active</a>
+        <a @click.prevent="setFilter('completed')" :class="{active:currentFilter=='completed'}">Completed</a>
+    </div>
+</template>
+
+<script>
+    import {mapState, mapMutations} from "vuex";
+
+    export default {
+        name: "todo-list-filter",
+        computed: {
+            ...mapState(['currentFilter'])
+        },
+        methods: {
+            ...mapMutations(['updateCurrentFilter']),
+            setFilter: function (currentFilter) {
+                this.updateCurrentFilter(currentFilter);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
