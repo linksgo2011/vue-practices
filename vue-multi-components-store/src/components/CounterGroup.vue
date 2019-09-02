47ed151c669fb5ff004394a55a6d85c2bcf978ad
Lin Ning <nlin@thoughtworks.com>
2019-09-02 15:33:52 +0800

    refactor with vuex

---
+++ vue-multi-components-store/src/components/CounterGroup.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="counter-group">
+        <h2>
+            Counter Number:
+            <span>{{ counterNumber }}</span>
+        </h2>
+        <div class="counters">
+            <Counter
+                    v-for="(counter,index) in counters"
+                    :index="index"
+                    :value="counter.value"
+                    :key="index"
+            />
+        </div>
+        <CounterSum></CounterSum>
+    </div>
+</template>
+
+<script>
+    import Counter from './Counter.vue'
+    import CounterSum from './CounterSum.vue'
+
+    export default {
+        name: 'counter-group',
+        components: {
+            Counter,
+            CounterSum
+        },
+        computed: {
+            counterNumber: function () {
+                return this.$store.state.counterNumbers;
+            },
+            counters: function(){
+                return this.$store.state.counters;
+            }
+        }
+    }
+</script>
