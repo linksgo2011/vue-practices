524ada74b200eb9aade30e2dd2de0deb1dcb14b2
Lin Ning <nlin@thoughtworks.com>
2019-09-02 18:04:29 +0800

    add vue todo list vuex version

---
+++ vue-todo-list-vuex/src/components/CreateForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="input-form">
+        <input type="input" v-model="inputtingItem">
+        <button @click="createTodo(inputtingItem)">Add</button>
+    </div>
+</template>
+
+<script>
+    import {mapMutations} from "vuex";
+
+    export default {
+        name: "create-form",
+        data: function () {
+            return {
+                inputtingItem: ""
+            }
+        },
+        methods: {
+            ...mapMutations(['createTodo'])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
